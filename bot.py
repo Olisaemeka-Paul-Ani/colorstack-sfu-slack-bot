7d7e8a469fe5274f14c76cdef261f0296bb6be69
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-04-22 02:59:40 -0700

    feat: add LeetCode daily question posting to #leetcode-grind

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from dotenv import load_dotenv
@@ -26,10 +27,58 @@ def handle_member_joined(event, client):
         channel=user_id,
         text=welcome_message
     )
-    
+
+
     print(f"Sent welcome message to {user_name}")
 
+
+def fetch_leetcode_daily():
+    url = "https://leetcode.com/graphql"
+    
+    query = """
+    query questionOfToday {
+    activeDailyCodingChallengeQuestion {
+        date
+        link
+        question {
+        title
+        difficulty
+        }
+    }
+    }
+    """
+    payload = {"query": query}
+    response = requests.post(url, json=payload)
+    data = response.json()
+    title = data["data"]["activeDailyCodingChallengeQuestion"]["question"]["title"]
+    link = data["data"]["activeDailyCodingChallengeQuestion"]["link"]
+    difficulty = data["data"]["activeDailyCodingChallengeQuestion"]["question"]["difficulty"]
+
+    hashMap = {
+        "title": title,
+        "link": f"https://leetcode.com{link}",
+        "difficulty": difficulty
+    }
+
+    return hashMap
+def post_leetcode_question(hMap):
+    title = hMap["title"]
+    link = hMap["link"]
+    difficulty = hMap["difficulty"]
+    message = f"""
+    
+    *Today's LeetCode Question -- {difficulty}*
+    {title}
+    {link}
+
+                        """
+    app.client.chat_postMessage(
+    channel="leetcode-grind",
+    text=message
+    )
+    return message
 if __name__ == "__main__":
+    print(post_leetcode_question(fetch_leetcode_daily()))
     print("⚡️ Bot starting in Socket Mode...")
     try:
         handler = SocketModeHandler(app, app_token)
@@ -37,3 +86,4 @@ if __name__ == "__main__":
         handler.start()
     except Exception as e:
         print(f"Error starting bot: {e}")
+    
